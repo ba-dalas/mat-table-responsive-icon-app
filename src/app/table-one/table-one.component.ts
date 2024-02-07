871e93e8f721fc16aca37a99bbedaa70ef00aeb8
ba-dalas <rumi@ba-systems.com>
2024-02-07 15:19:25 +0600

    update

--- src/app/table-one/table-one.component.ts
+++ src/app/table-one/table-one.component.ts
@@ -12,12 +12,12 @@ const ELEMENT_DATA: PeriodicElement[] = [
   // {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
   // {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
   // {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 5, name: 'Borongdfgdfgdfgdfgdfgdfg', weight: 10.811, symbol: ''},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: ''},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: ''},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: ''},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: ''},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: ''},
 ];
 
 @Component({
@@ -27,7 +27,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class TableOneComponent {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'symbol', 'weight'];
   dataSource = ELEMENT_DATA;
 
 }
